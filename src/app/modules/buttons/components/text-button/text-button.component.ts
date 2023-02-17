f90a54a497247c3bc4a5c05b7f37cbecd1c4c2e3
LucasiBai <lucasibaigorria@mi.unc.edu.ar>
2023-02-16 23:34:16 -0300

    agregado de color dinamico en color con color standar si no se le asigna

--- src/app/modules/buttons/components/text-button/text-button.component.ts
+++ src/app/modules/buttons/components/text-button/text-button.component.ts
@@ -1,10 +1,37 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-text-button',
   templateUrl: './text-button.component.html',
   styleUrls: ['./text-button.component.css'],
 })
-export class TextButtonComponent {
-  @Input() color!: string;
+export class TextButtonComponent implements OnInit {
+  color!: string;
+  @Input() hue!: string;
+  @Input() saturation!: string;
+  @Input() lightness!: string;
+
+  ngOnInit() {
+    this.initColor();
+  }
+
+  initColor() {
+    const hue = this.hue ? this.hue : '213';
+    const saturation = this.saturation ? this.saturation : '10';
+    const lightness = this.lightness ? this.lightness : '45';
+
+    this.color = `hsl(${hue}, ${saturation}%, ${lightness}%)`;
+  }
+
+  setHoverLightness() {
+    const hue = this.hue ? this.hue : '213';
+    const saturation = this.saturation ? this.saturation : '10';
+    const lightness = this.lightness ? this.lightness : '45';
+
+    this.color = `hsl(${hue}, ${saturation}%, ${Number(lightness) - 10}%)`;
+  }
+
+  setLeaveLightness() {
+    this.initColor();
+  }
 }
