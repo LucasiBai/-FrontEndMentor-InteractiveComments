219121c922305a0397faabca11094c60dec152f1
LucasiBai <lucasibaigorria@mi.unc.edu.ar>
2023-03-03 17:29:43 -0300

    agregando tests a comment card

---
+++ src/app/modules/comments/components/comment-card/comment-card.component.spec.ts
@@ -0,0 +1,51 @@
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { CommentI } from 'src/app/modules/data/models/comment-i';
+import { ReplyButtonComponent } from '../buttons/reply-button/reply-button.component';
+import { CommentContentComponent } from '../comment-content/comment-content.component';
+import { InteractiveButtonsComponent } from '../interactive-buttons/interactive-buttons.component';
+import { ScoreCounterComponent } from '../score-counter/score-counter.component';
+import { UserIconComponent } from '../user-icon/user-icon.component';
+import { CommentCardComponent } from './comment-card.component';
+
+const mockCommentCard: CommentI = {
+  id: 1,
+  content:
+    "Impressive! Though it seems the drag feature could be improved. But overall it looks incredible. You've nailed the design and the responsiveness at various breakpoints works really well.",
+  createdAt: '1 month ago',
+  score: 12,
+  user: {
+    image: {
+      png: './images/avatars/image-amyrobson.png',
+      webp: './images/avatars/image-amyrobson.webp',
+    },
+    username: 'amyrobson',
+  },
+  replies: [],
+};
+
+describe('Test Comment Card', () => {
+  let fixture: ComponentFixture<CommentCardComponent>;
+  let component: CommentCardComponent;
+
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      declarations: [
+        CommentCardComponent,
+        ScoreCounterComponent,
+        UserIconComponent,
+        InteractiveButtonsComponent,
+        CommentContentComponent,
+        ReplyButtonComponent,
+      ],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(CommentCardComponent);
+    component = fixture.componentInstance;
+    component.comment = mockCommentCard;
+    fixture.detectChanges();
+  }));
+
+  it('Should render comment card', () => {
+    expect(component).toBeTruthy();
+  });
+});
