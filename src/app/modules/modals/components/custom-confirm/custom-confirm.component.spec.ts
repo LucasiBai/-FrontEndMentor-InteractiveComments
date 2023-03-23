bddcdf768de91efe13dcd3c4ebbc93f0b423a053
LucasiBai <lucasibaigorria@mi.unc.edu.ar>
2023-03-23 07:21:06 -0300

    tests creados y pasados en confirm modal

--- src/app/modules/modals/components/custom-confirm/custom-confirm.component.spec.ts
+++ src/app/modules/modals/components/custom-confirm/custom-confirm.component.spec.ts
@@ -1,6 +1,16 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { ButtonsModule } from 'src/app/modules/buttons/buttons.module';
+import { ConfirmI } from '../../models/confirm-i';
 import { CustomConfirmComponent } from './custom-confirm.component';
+
+let exucuted = false;
+
+const confirmData: ConfirmI = {
+  isConfirming: true,
+  action: () => (exucuted = true),
+  header: 'Test Header',
+  content: '',
+};
 
 describe('Test Custom Confirm Modal', () => {
   let fixture: ComponentFixture<CustomConfirmComponent>;
@@ -26,5 +36,95 @@ describe('Test Custom Confirm Modal', () => {
     const domComponent =
       fixture.debugElement.nativeElement.querySelector('.modal__box');
     expect(domComponent).toBeFalsy();
+  });
+
+  describe('Test render modal', () => {
+    beforeEach(() => {
+      component.confirmData = confirmData;
+      fixture.detectChanges();
+    });
+
+    it('Should render modal with confirm data', () => {
+      const domComponent =
+        fixture.debugElement.nativeElement.querySelector('.modal__box');
+      expect(domComponent).toBeTruthy();
+
+      const h5 = fixture.debugElement.nativeElement.querySelector('h5');
+      expect(h5).toBeTruthy();
+      expect(h5.innerText).toEqual(confirmData.header);
+
+      const p = fixture.debugElement.nativeElement.querySelector('p');
+      expect(p).toBeFalsy();
+    });
+
+    it('Should render modal with content', () => {
+      const confirmDataContent = { ...confirmData, content: 'Test Content' };
+      component.confirmData = confirmDataContent;
+      fixture.detectChanges();
+
+      const domComponent =
+        fixture.debugElement.nativeElement.querySelector('.modal__box');
+      expect(domComponent).toBeTruthy();
+
+      const h5 = fixture.debugElement.nativeElement.querySelector('h5');
+      expect(h5).toBeTruthy();
+      expect(h5.innerText).toEqual(confirmData.header);
+
+      const p = fixture.debugElement.nativeElement.querySelector('p');
+      expect(p).toBeTruthy();
+      expect(p.innerText).toEqual(confirmDataContent.content);
+    });
+  });
+
+  describe('Test closeModal()', () => {
+    beforeEach(() => {
+      component.confirmData = confirmData;
+      fixture.detectChanges();
+    });
+
+    it('Should close open modal', () => {
+      const domTreeOpen =
+        fixture.debugElement.nativeElement.querySelector('.modal__box');
+      expect(domTreeOpen).toBeTruthy();
+
+      component.closeModal();
+      fixture.detectChanges();
+
+      const domTreeClose =
+        fixture.debugElement.nativeElement.querySelector('.modal__box');
+      expect(domTreeClose).toBeFalsy();
+    });
+  });
+
+  describe('Test executeAction()', () => {
+    beforeEach(() => {
+      component.confirmData = confirmData;
+      fixture.detectChanges();
+    });
+
+    it('Should execute modal data action', () => {
+      const domTreeOpen =
+        fixture.debugElement.nativeElement.querySelector('.modal__box');
+      expect(domTreeOpen).toBeTruthy();
+
+      component.executeAction();
+
+      expect(exucuted).toBeTrue();
+      exucuted = false;
+    });
+
+    it('Should close modal at finish', () => {
+      const domTreeOpen =
+        fixture.debugElement.nativeElement.querySelector('.modal__box');
+      expect(domTreeOpen).toBeTruthy();
+
+      component.executeAction();
+      fixture.detectChanges();
+
+      const domTreeClose =
+        fixture.debugElement.nativeElement.querySelector('.modal__box');
+      expect(domTreeClose).toBeFalsy();
+      exucuted = false;
+    });
   });
 });
